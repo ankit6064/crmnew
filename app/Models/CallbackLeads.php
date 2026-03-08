87c29b70cf049fd504cc64a25bf199990081b6cf
Ankit Jain <ankitjain@192.168.1.6>
2026-03-08 16:49:22 +0530

    fixcallback

--- app/Models/CallbackLeads.php
+++ app/Models/CallbackLeads.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class CallbackLeads extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'note_id',
+        'employee_id',
+        'lead_id',
+        'callback_date',
+        'callback_time',
+    ];
 }
